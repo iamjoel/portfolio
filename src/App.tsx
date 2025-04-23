fe7b7bca74d69ec649f93d331d882a7b3d729bc7
Joel <iamjoel007@gmail.com>
2025-04-23 13:01:48 +0800

    feat: detail back to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import GithubCorner from './components/GithubCorner';
